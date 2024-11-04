91324c87ea48df22e0748f9a7f4c36478389018f
Sandra Tang <tangsandra9@gmail.com>
2024-11-04 18:01:33 -0500

    Create FileReader.java

---
+++ src/cs3500/threetrios/controller/FileReader.java
@@ -0,0 +1,13 @@
+package cs3500.threetrios.controller;
+
+public class FileReader {
+
+  public static String cardFileReader(String filename) {
+    return "";
+  }
+
+  public static String gridFileReader(String filename) {
+    return "";
+  }
+
+}
