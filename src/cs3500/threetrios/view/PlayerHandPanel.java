19e486a775b07777ab9d26c628ba4ff25190647a
Sandra Tang <tangsandra9@gmail.com>
2024-11-05 20:55:23 -0500

    Added printing of card index and color

--- src/cs3500/threetrios/view/PlayerHandPanel.java
+++ src/cs3500/threetrios/view/PlayerHandPanel.java
@@ -9,20 +9,26 @@ import cs3500.threetrios.model.ThreeTriosCard;
 
 public class PlayerHandPanel extends JPanel {
   private CellPanel selectedCardPanel = null;
+  private List<ThreeTriosCard> cards;
+  private PlayerColor color;
 
   public PlayerHandPanel(List<ThreeTriosCard> cards, PlayerColor color) {
+    this.cards = cards; // Store the list of cards
+    this.color = color; // Store the player color
     setLayout(new GridLayout(cards.size(), 1)); // Vertical layout
 
-    for (ThreeTriosCard card : cards) {
+    for (int i = 0; i < cards.size(); i++) {
+      ThreeTriosCard card = cards.get(i);
       CellPanel cardPanel = new CellPanel(card, color); // Use CellPanel for card display
       cardPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY));
 
       // Set preferred size for the card panel
       cardPanel.setPreferredSize(new Dimension(100, 150)); // Adjust width and height as needed
 
+      int cardIndex = i; // Final variable to pass to the listener
       cardPanel.addMouseListener(new java.awt.event.MouseAdapter() {
         public void mouseClicked(java.awt.event.MouseEvent evt) {
-          handleCardSelection(cardPanel);
+          handleCardSelection(cardPanel, cardIndex);
         }
       });
 
@@ -30,7 +36,7 @@ public class PlayerHandPanel extends JPanel {
     }
   }
 
-  private void handleCardSelection(CellPanel cardPanel) {
+  private void handleCardSelection(CellPanel cardPanel, int cardIndex) {
     if (selectedCardPanel != null) {
       selectedCardPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY));
     }
@@ -41,6 +47,9 @@ public class PlayerHandPanel extends JPanel {
       cardPanel.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 3));
       selectedCardPanel = cardPanel;
     }
+
+    // Print the index and player color
+    System.out.println("Card index: " + cardIndex + ", Player color: " + color);
 
     revalidate();
     repaint();
